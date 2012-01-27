4036d4e5dc2224b2ec348a55bd46d0cb617cd6d8
Tim Santeford <tsantef@gmail.com>
2012-01-27 14:56:57 -0800

    thumb test

--- thumb.php
+++ thumb.php
@@ -3,8 +3,9 @@
 	require "vendor/.composer/autoload.php";
 
 	$imagine = new \Imagine\Gd\Imagine();
-	$image = $imagine->open('assets/logo-large.png');
-	$thumbnail = $image->thumbnail(new Imagine\Image\Box(100, 100));
-	$thumbnail->save('assets/example.thumb.png');
+	//$image = $imagine->open('assets/logo-large.png');
+	//$thumbnail = $image->thumbnail(new Imagine\Image\Box(100, 100));
+	//$thumbnail->save('assets/example.thumb.png');
 
 ?>
+TEST
